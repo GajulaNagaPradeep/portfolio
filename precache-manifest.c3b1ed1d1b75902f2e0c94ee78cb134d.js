85de6ed430dfe7d0eafa44df428c7343bdedf283
GajulaNagaPradeep <Naga.Pradeep.Gajula@dhcs.ca.gov>
2025-03-18 00:40:25 -0700

    Updates

---
+++ precache-manifest.c3b1ed1d1b75902f2e0c94ee78cb134d.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02034b0928cef0d8ab958c5c61d68955",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0aa1793f8a24427058bc",
+    "url": "/portfolio/static/css/main.b1dbc65b.chunk.css"
+  },
+  {
+    "revision": "2a017245be1ccb38c33b",
+    "url": "/portfolio/static/js/2.9183d46d.chunk.js"
+  },
+  {
+    "revision": "c21d579849576bf6a47550572ff70f62",
+    "url": "/portfolio/static/js/2.9183d46d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa1793f8a24427058bc",
+    "url": "/portfolio/static/js/main.c76c6610.chunk.js"
+  },
+  {
+    "revision": "b1376ee2c8cfa7e6ec93",
+    "url": "/portfolio/static/js/runtime-main.47080001.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "3da3ad80b621d42560cd1f80e32639e1",
+    "url": "/portfolio/static/media/IJCST.3da3ad80.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "017074450807dbd55fa14c4d27f54398",
+    "url": "/portfolio/static/media/bsnl.01707445.avif"
+  },
+  {
+    "revision": "7464b96b6775b40083adc581027908a5",
+    "url": "/portfolio/static/media/campUniv.7464b96b.png"
+  },
+  {
+    "revision": "a0905408f68b51700fa53a8c0154468a",
+    "url": "/portfolio/static/media/cwds.a0905408.webp"
+  },
+  {
+    "revision": "404ae7bc061a03e399daa61344c78566",
+    "url": "/portfolio/static/media/cwoc.404ae7bc.png"
+  },
+  {
+    "revision": "f0f0357fd892ace9072e15478e43afa0",
+    "url": "/portfolio/static/media/deepu.f0f0357f.png"
+  },
+  {
+    "revision": "750bc05ebe0cc2b289d4eb729526e5f9",
+    "url": "/portfolio/static/media/dhcs.750bc05e.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/portfolio/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/portfolio/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/portfolio/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/portfolio/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/portfolio/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "e133a5afcdeb7a6c2eb248a90a0e3534",
+    "url": "/portfolio/static/media/nextera.e133a5af.jpeg"
+  },
+  {
+    "revision": "22f11067f2d745aaceb51e485720635d",
+    "url": "/portfolio/static/media/postman.22f11067.jpg"
+  },
+  {
+    "revision": "c108f55777ceaa6690606a207b737d0a",
+    "url": "/portfolio/static/media/postman.c108f557.png"
+  },
+  {
+    "revision": "20b3f6c6c15bad41b912f604f2856f49",
+    "url": "/portfolio/static/media/safe.20b3f6c6.png"
+  },
+  {
+    "revision": "1a125295d31f1223535f06f67e981d7a",
+    "url": "/portfolio/static/media/univofDenver.1a125295.png"
+  },
+  {
+    "revision": "c61705bed1161fc479f8195d55044b68",
+    "url": "/portfolio/static/media/usbank.c61705be.png"
+  },
+  {
+    "revision": "edfdb1dea1016f73ae18c41cb80502bb",
+    "url": "/portfolio/static/media/usbank.edfdb1de.jpg"
+  }
+]);
